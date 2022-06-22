39b588a6fa8eee5ab3c8e79406e623f8c1532553
Ta Quoc Niem <niem91cm@gmail.com>
2022-06-22 23:45:07 +0700

    cai dat cac route cho camtac

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./routes/contact.route");
+const ApiError = require("./api-error");
 
 const app = express();
 
@@ -8,6 +10,18 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({ message: "Xin chao den voi ung dung Quan Ly danh ba." });
+});
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
 });
 
 module.exports = app;
--- config/index.js
+++ config/index.js
@@ -2,7 +2,9 @@ const config = {
     app: {
         port: 3000,
     },
-    
+    db: {
+        uri: "mongodb://127.0.0.1:27017/demolab-be",
+    },
 };
 
 module.exports = config;
